52975b05983c99ca6fb1fe58ef1831b71823a470
Gabriel Winkler <999gabriel.winkler@gmail.com>
2025-09-17 18:55:52 +0200

    C# code from lesson with Enum implementation in Article.cs

---
+++ Csharp-code-school/Category.cs
@@ -0,0 +1,14 @@
+namespace Csharp_code_school;
+
+// Defines the available product categories for articles
+// Used to classify and organize inventory items
+
+public enum Category
+{
+    Book,
+    Shoes,
+    Computer,
+    Handy,
+    Laptop,
+    NotSpecified
+}
